840ee3fb8866545c30f78336b198985feae7ccb6
anderskloborg <anderskloborg@gmail.com>
2023-04-11 20:48:09 +0000

    rewrite scanner to use match

--- src/scanner.rs
+++ src/scanner.rs
@@ -39,42 +39,70 @@ impl Scanner {
 
     fn scan_token(&mut self, tokens: &mut Vec<Token>) {
         if let Some(c) = self.advance() {
-            if Token::is_always_single_character_token(c) && let Ok(token_type) = TokenType::try_from(c) {
-                self.add_token(tokens, token_type);
-            } else if Token::is_always_single_or_double_character_token(c) {
-                let token_type = match c {
-                    '!' => if self.match_current('=') {TokenType::BangEqual} else  {TokenType::Bang },
-                    '=' => if self.match_current('=') {TokenType::EqualEqual } else  {TokenType::Equal },
-                    '<' => if self.match_current('=') {TokenType::LessEqual } else  {TokenType::Less },
-                    '>' => if self.match_current('=') {TokenType::GreaterEqual} else  {TokenType::Greater },
-                    _ => unreachable!()
-                };
-
-                self.add_token(tokens, token_type);
-            } else if c == '/' {
-                if self.match_current('/') {
-                    while self.peek().is_some() && !self.is_at_end() {
-                        _ = self.advance();
+            match c {
+                // Always single character
+                '(' | ')' | '{' | '}' | ',' | '.' | '-' | '+' | ';' | '*' => {
+                    if let Ok(token_type) = TokenType::try_from(c) {
+                        self.add_token(tokens, token_type);
+                    } else {
+                        self.errors.push(FeroxError::SyntaxError {
+                            error_description: "Unexpected character".to_owned(),
+                            line_number: self.line_number,
+                        });
                     }
-
-                    // Handle comment
-                } else {
-                    self.add_token(tokens, TokenType::Slash);
                 }
-            } else if self.should_ignore(c) {
-                if c == '\n' {
-                    self.line_number += 1;
+                // Always single or double character
+                '!' | '=' | '<' | '>' => {
+                    if let Some(token_type) = self.single_or_double_character_token_type(c) {
+                        self.add_token(tokens, token_type);
+                    } else {
+                        self.errors.push(FeroxError::SyntaxError {
+                            error_description: "Unexpected character".to_owned(),
+                            line_number: self.line_number,
+                        });
+                    }
                 }
-            } else {
-                self.errors.push(FeroxError::SyntaxError {
-                    error_description: "Unexpected character".to_owned(),
-                    line_number: self.line_number,
-                });
-            };
+                // Start of comment or slash
+                '/' => {
+                    if self.match_current('/') {
+                        while self.peek().is_some() && !self.is_at_end() {
+                            _ = self.advance();
+                        }
+                    } else {
+                        self.add_token(tokens, TokenType::Slash);
+                    }
+                }
+                // Ignore
+                ' ' | '\r' | '\t' | '\n' => {
+                    if c == '\n' {
+                        self.line_number += 1;
+                    }
+                }
+                // Start of string literal
+                '"' => {
+                    self.handle_string_literal(tokens);
+                }
+                _ => {
+                    self.errors.push(FeroxError::SyntaxError {
+                        error_description: "Unexpected character".to_owned(),
+                        line_number: self.line_number,
+                    });
+                }
+            }
         }
     }
 
     fn add_token(&self, tokens: &mut Vec<Token>, token_type: TokenType) {
+        let token = Token::new(
+            token_type,
+            self.source[self.start..self.current].iter().collect(),
+            self.line_number,
+        );
+
+        tokens.push(token);
+    }
+
+    fn add_token_literal(&self, tokens: &mut Vec<Token>, token_type: TokenType) {
         let token = Token::new(
             token_type,
             self.source[self.start..self.current].iter().collect(),
@@ -115,5 +143,55 @@ impl Scanner {
 
     fn should_ignore(&self, c: char) -> bool {
         matches!(c, ' ' | '\r' | '\t' | '\n')
+    }
+
+    fn handle_string_literal(&mut self, tokens: &mut Vec<Token>) {
+        while let Some(c) = self.peek() && c != '"' && !self.is_at_end() {
+            if c == '\n' {
+                self.line_number += 1;
+            }
+            self.advance();
+        }
+
+        if self.is_at_end() {
+            self.errors.push(FeroxError::SyntaxError {
+                error_description: "Unterminated string".to_owned(),
+                line_number: self.line_number,
+            })
+        }
+
+        self.advance();
+
+        let value: String = self.source[self.start + 1..self.current - 1]
+            .iter()
+            .collect();
+
+        self.add_token(tokens, TokenType::String { value });
+    }
+
+    fn single_or_double_character_token_type(&mut self, c: char) -> Option<TokenType> {
+        match c {
+            '!' => Some(if self.match_current('=') {
+                TokenType::BangEqual
+            } else {
+                TokenType::Bang
+            }),
+            '=' => Some(if self.match_current('=') {
+                TokenType::EqualEqual
+            } else {
+                TokenType::Equal
+            }),
+            '<' => Some(if self.match_current('=') {
+                TokenType::LessEqual
+            } else {
+                TokenType::Less
+            }),
+            '>' => Some(if self.match_current('=') {
+                TokenType::GreaterEqual
+            } else {
+                TokenType::Greater
+            }),
+            _ => None,
+        }
     }
 }
